a83912e3ec74a9302a703b1502cd0028c8a49427
Jom-Winston <bjomwinston0107@gmail.com>
2023-06-11 20:04:20 +0800

    UnitAssets & exit

---
+++ Pong_Project/Assets/script/ExitManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitManager : MonoBehaviour
+{
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+    
+}
