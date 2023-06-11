f39ceac83c3b794d44be6a0cb2b33d353913a3fc
Jom-Winston <bjomwinston0107@gmail.com>
2023-06-11 14:55:53 +0800

    Deisgn-main

---
+++ Pong_Project/Assets/script/Scenehandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenehandler : MonoBehaviour
+{
+   public  void register()
+    {
+        SceneManager.LoadScene("scn_register");
+    }
+
+
+}
